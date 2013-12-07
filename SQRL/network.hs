59b7bd20c3000c08f7cf3990b76d108038f9f002
nearsyh <nearsy.h@gmail.com>
2013-12-07 13:49:01 -0500

    updating the main.hs

--- SQRL/network.hs
+++ SQRL/network.hs
@@ -15,9 +15,11 @@ import Data.Default
 login :: ByteString -> String -> String -> IO (Response LB.ByteString)
 login masterKey password url = do
     let (scheme, domain, path, query, challenge) = SURL.parseUrl url
+    print domain
     let (privateKey, publicKey) = genKeyPair masterKey password domain
     let (scheme, head, query) = SURL.getUrl url 1 [] publicKey (pack "")
     let (sig, pub) = genSigAndPub (privateKey, publicKey) (scheme ++ "//" ++ query)
+    print sig
     issueRequest (head ++ "//" ++ query) sig
 
 getRequest :: (Monad m) => String -> String -> IO (Request m)
@@ -25,6 +27,7 @@ getRequest url body = do
     initReq <- parseUrl url
     let req' = initReq { secure = True, method = methodPost }
     let req = (flip urlEncodedBody) req' $ [((pack "sqrlsig"), (pack body))]
+    print req
     return req
 
 issueRequest :: String -> String -> IO (Response LB.ByteString)
